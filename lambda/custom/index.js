9d4c300c0547d35f98873c9d228b5b7574167115
Brady Holt <brady.holt@gmail.com>
2017-10-12 22:51:50 -0700

    step4 buildout

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -15,11 +15,5 @@ var handlers = {
     'PlayChord': function () {
         let chord = resources.getChord("C", "major");
         this.emit(':tellWithCard', `<audio src="${chord.audioUri}"/>`, chord.name, chord.name, { largeImageUrl: chord.imageUri, smallImageUrl: chord.imageUri });
-    },
-    'AMAZON.HelpIntent': function () {
-        this.emit(':tell', 'Say play a chord.');
-    },
-    'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't understand that.");
     }
 };
